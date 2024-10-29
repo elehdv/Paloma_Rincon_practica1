4e7c6efce94d32f2287f8d8318aa57b68f9853af
mdltm <imaginamartina@gmail.com>
2024-10-29 11:24:19 +0100

    home js (typed.js library)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed2 = new Typed('#typed2', {
+    strings: ['Hi! Im Paloma. I am a visual artist working in lens based still and moving image projects in a quirky style marked by bold colors and graphic compositions.'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    fadeOut: true
+    
+  });
