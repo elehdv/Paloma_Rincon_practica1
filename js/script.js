fbf7fb834f564c2ce6e38e90769fa2ccf1622e8f
mdltm <imaginamartina@gmail.com>
2024-10-29 13:44:08 +0100

    project intro title $ bouncing arrow

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-var typed2 = new Typed('#typed2', {
+let $document = $(document)
+$document.ready(function(){
+    
+    var typed2 = new Typed('#typed2', {
     strings: ['Hi! Im Paloma. I am a visual artist working in lens based still and moving image projects in a quirky style marked by bold colors and graphic compositions.'],
     typeSpeed: 0,
     backSpeed: 0,
     fadeOut: true
     
   });
+
+
+});
