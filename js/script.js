881300ac7b385aa966ea5dc3d00c07450abdb64e
mdltm <imaginamartina@gmail.com>
2024-11-07 23:05:59 +0100

    project guide

--- js/script.js
+++ js/script.js
@@ -11,6 +11,52 @@ $document.ready(function(){
 
   AOS.init();
 
+  $("#projects-guide > div > a").on("click", function(){
+    //console.log($("#projects-guide-open").css("display"));
+    //console.log($(".fa-solid.fa-chevron-up").css("display"));
+  
+      let actual_chevron = $("#projects-guide-open").css("display");
+  
+      if (actual_chevron === "none") {
+         $("#projects-guide-open").css("display", "flex");
+              
+      } else {
+        $("#projects-guide-open").css("display", "none");
+      }
+  
+  });
+
+  $("#project-menu-cross").on("click", function(){
+    //console.log($("#projects-guide-open").css("display"));
+    //console.log($(".fa-solid.fa-chevron-up").css("display"));
+  
+      let actual_chevron = $("#projects-guide-open").css("display");
+  
+      if (actual_chevron === "none") {
+         $("#projects-guide-open").css("display", "flex");
+              
+      } else {
+        $("#projects-guide-open").css("display", "none");
+      }
+  
+  });
+
+  $("#projects-guide-open > a").on("click", function(){
+    //console.log($("#projects-guide-open").css("display"));
+    //console.log($(".fa-solid.fa-chevron-up").css("display"));
+  
+      let actual_chevron = $("#projects-guide-open").css("display");
+  
+      if (actual_chevron === "none") {
+         $("#projects-guide-open").css("display", "flex");
+              
+      } else {
+        $("#projects-guide-open").css("display", "none");
+      }
+  
+  });
+
+
 
 });
 
