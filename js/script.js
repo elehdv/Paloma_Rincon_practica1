3a811221fca36e94f73641dd0903274fa52b865c
mdltm <imaginamartina@gmail.com>
2024-11-10 16:51:43 +0100

    home page responsive & menu in all pages

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 let $document = $(document)
 $document.ready(function(){
 
-    
-    var typed2 = new Typed('#typed2', {
-    strings: ['Hi! Im Paloma. I am a visual artist working in lens based still and moving image projects in a quirky style marked by bold colors and graphic compositions.'],
-    typeSpeed: 0,
-    backSpeed: 0,
-    fadeOut: true
-    
-  });
+
 
   AOS.init();
 
@@ -136,6 +129,16 @@ $document.ready(function(){
         $("#menu-open").css("opacity", "1");
       }
   
+  });
+
+
+      
+  var typed2 = new Typed('#typed2', {
+    strings: ['Hi! Im Paloma. I am a visual artist working in lens based still and moving image projects in a quirky style marked by bold colors and graphic compositions.'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    fadeOut: true
+    
   });
 
 
